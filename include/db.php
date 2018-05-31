9023d3f692cba53d67252d3b172206210dc12dfd
Luu Cuong <cuong.luusean@gmail.com>
2018-05-31 15:52:45 +0700

    updated sth

--- include/db.php
+++ include/db.php
@@ -1,7 +1,7 @@
 <?php
 	$servername = "localhost";
 	$username = "root";
-	$password = "";
+	$password = "kkkkkk";
 	$database = "qlkhachsan";
 	$connect = mysqli_connect($servername, $username, $password) or die("Could not connect DB");
 	mysqli_select_db($connect, $database) or die("Could not find DB!");
